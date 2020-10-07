05ecbfa58481a74b1ae0de6777c3bedb7a155d92
Oscar Mier <voscarmv@gmail.com>
2020-10-06 21:26:21 -0500

    Added button panel.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Display from './Display';
+import ButtonPanel from './ButtonPanel';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <h1>Calcoolator</h1>
       <Display />
+      <ButtonPanel />
     </div>
   );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
+import React from 'react';
+
+const Button = props => (
+  <button type="button">
+    {props.name}
+  </button>
+);
+
+export default Button;
