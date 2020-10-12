5e7a36eaf4a5ec11b0677a8c7807714d80c58cdd
Oscar Mier <voscarmv@gmail.com>
2020-10-12 13:07:24 -0500

    Prepared buttonPanel to handle clicks.

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,117 +1,138 @@
 import React from 'react';
 import Button from './Button';
+import PropTypes from 'prop-types';
 
-const buttonGroups = [
-  {
-    buttons: [
+class ButtonPanel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.buttonGroups = [
       {
-        name: 'AC',
-        color: 'lightgray',
+        buttons: [
+          {
+            name: 'AC',
+            color: 'lightgray',
+          },
+          {
+            name: '+/,-',
+            color: 'lightgray',
+          },
+          {
+            name: '%',
+            color: 'lightgray',
+          },
+          {
+            name: '÷',
+          },
+        ],
       },
       {
-        name: '+/,-',
-        color: 'lightgray',
+        buttons: [
+          {
+            name: '7',
+            color: 'lightgray',
+          },
+          {
+            name: '8',
+            color: 'lightgray',
+          },
+          {
+            name: '9',
+            color: 'lightgray',
+          },
+          {
+            name: 'X',
+          },
+        ],
       },
       {
-        name: '%',
-        color: 'lightgray',
+        buttons: [
+          {
+            name: '4',
+            color: 'lightgray',
+          },
+          {
+            name: '5',
+            color: 'lightgray',
+          },
+          {
+            name: '6',
+            color: 'lightgray',
+          },
+          {
+            name: '-',
+          },
+        ],
       },
       {
-        name: '÷',
-      },
-    ],
-  },
-  {
-    buttons: [
-      {
-        name: '7',
-        color: 'lightgray',
-      },
-      {
-        name: '8',
-        color: 'lightgray',
-      },
-      {
-        name: '9',
-        color: 'lightgray',
-      },
-      {
-        name: 'X',
-      },
-    ],
-  },
-  {
-    buttons: [
-      {
-        name: '4',
-        color: 'lightgray',
+        buttons: [
+          {
+            name: '1',
+            color: 'lightgray',
+          },
+          {
+            name: '2',
+            color: 'lightgray',
+          },
+          {
+            name: '3',
+            color: 'lightgray',
+          },
+          {
+            name: '+',
+          },
+        ],
       },
       {
-        name: '5',
-        color: 'lightgray',
+        buttons: [
+          {
+            name: '0',
+            wide: true,
+            color: 'lightgray',
+          },
+          {
+            name: '.',
+            color: 'lightgray',
+          },
+          {
+            name: '=',
+          },
+        ],
       },
-      {
-        name: '6',
-        color: 'lightgray',
-      },
-      {
-        name: '-',
-      },
-    ],
-  },
-  {
-    buttons: [
-      {
-        name: '1',
-        color: 'lightgray',
-      },
-      {
-        name: '2',
-        color: 'lightgray',
-      },
-      {
-        name: '3',
-        color: 'lightgray',
-      },
-      {
-        name: '+',
-      },
-    ],
-  },
-  {
-    buttons: [
-      {
-        name: '0',
-        wide: true,
-        color: 'lightgray',
-      },
-      {
-        name: '.',
-        color: 'lightgray',
-      },
-      {
-        name: '=',
-      },
-    ],
-  },
-];
+    ];
+  }
 
-const ButtonPanel = () => (
-  <div className="ButtonPanel">
-    {
-      buttonGroups.map(
-        g => (
-          <div className="buttonrow" key={g.id}>
-            {
-              g.buttons.map(
-                b => <Button ButtonName={b.name} color={b.color} wide={b.wide} key={b.id} />,
-              )
-            }
-          </div>
-        ),
-      )
-    }
-  </div>
-);
+  handleClick = buttonName => {
+    const { clickHandler } = this.props;
+    clickHandler(buttonName);
+  }
+
+  render() {
+    return (
+      <div className="ButtonPanel">
+        {
+          this.buttonGroups.map(
+            g => (
+              <div className="buttonrow" key={g.id}>
+                {
+                  g.buttons.map(
+                    b => <Button clickHandler={this.handleClick(b.name)} ButtonName={b.name} color={b.color} wide={b.wide} key={b.id} />,
+                  )
+                }
+              </div>
+            ),
+          )
+        }
+      </div>
+    );
+  }
+}
+
+ButtonPanel.defaultProps = {
+  clickHandler: () => {},
+};
+
+ButtonPanel.propTypes = {
+  clickHandler: PropTypes.func,
+};
 
 export default ButtonPanel;
