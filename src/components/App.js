21e3d99cdc05b456c3f2e325bde0997d344be982
Oscar Mier <voscarmv@gmail.com>
2020-10-12 12:36:59 -0500

    Prepared App.js to handle click events.

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,31 @@ import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import './App.css';
+import calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <div className="App">
-      <Display />
-      <ButtonPanel />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = buttonName => {
+      const result = calculate(this.state, buttonName);
+      this.setState(result);
+    };
+  }
+
+  render() {
+    const { total, next } = this.state;
+    return (
+      <div className="App">
+        <Display total={total} next={next} />
+        <ButtonPanel clickHandler={this.handleClick} />
+      </div>
+    );
+  }
 }
 
 export default App;
