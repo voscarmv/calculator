96947286ea02304c1030ea762b659661b7b4e0ad
Oscar Mier <voscarmv@gmail.com>
2020-10-13 06:30:04 -0500

    Allow next calculation after error.

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -4,7 +4,11 @@ const calculate = ({ total, next, operation }, buttonName) => {
   const isDigit = char => char >= '0' && char <= '9';
 
   if (isDigit(buttonName)) {
-    return { total, next: `${next || ''}${buttonName}`, operation };
+    let first = '';
+    if (next) {
+      [first] = next;
+    }
+    return { total, next: `${isDigit(first) ? next || '' : ''}${buttonName}`, operation };
   }
 
   switch (buttonName) {
